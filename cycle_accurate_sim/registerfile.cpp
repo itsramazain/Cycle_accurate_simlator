ba563ea3cd9a7273fb3dc31682b0c70fdf976d9f
itsramazain <104790048+itsramazain@users.noreply.github.com>
2024-11-01 19:16:08 +0300

    Update registerfile.cpp

--- cycle_accurate_sim/registerfile.cpp
+++ cycle_accurate_sim/registerfile.cpp
@@ -32,10 +32,35 @@ class RF
         cout << "Memory content has been written to RFcontent" << endl;
     }
 
-    bitset<32> readRS(bitset<8>){
-        
+    bitset<32> readRS(bitset<5> address){
+         int addr = static_cast<int>(address.to_ulong());
+        if (addr >= 0 && addr < 32) {
+            return mem[addr];
+        } else {
+            cerr << "Error: Address out of bounds" << endl;
+            return bitset<32>();
+        }
     }
 
+
+    bitset<32> readRT(bitset<5> address){
+         int addr = static_cast<int>(address.to_ulong());
+        if (addr >= 0 && addr < 32) {
+            return mem[addr];
+        } else {
+            cerr << "Error: Address out of bounds" << endl;
+            return bitset<32>();
+        }
+    }
+
+    void write(bitset<5> address, bitset<32> data) {
+        int addr = static_cast<int>(address.to_ulong());
+        if (addr >= 0 && addr < 32) {
+            mem[addr] = data;
+        } else {
+            cerr << "Error: Address out of bounds" << endl;
+        }
+    }
 
 
 
