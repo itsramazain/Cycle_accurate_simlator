efe4e5c093f8e61f7aba81c30ea056f67789d7a8
itsramazain <104790048+itsramazain@users.noreply.github.com>
2024-11-01 21:21:59 +0300

    Update processor.cpp

--- cycle_accurate_sim/processor.cpp
+++ cycle_accurate_sim/processor.cpp
@@ -10,17 +10,61 @@ using namespace std;
 struct {            
     bitset<32> Instruction;
     bitset<6>pcplus4;
-} FDreg;
+} IFIDreg;
+
+
 
 struct {            
     bitset<32> Instruction;
-    bitset<6>pcplus4;
+    
+    bitset<1>Branch;
+    bitset<1>Zero;
+    bitset<1>MemRead;
+    bitset<1>MemWrite;
+
+    bitset<1> Regwwrite;
+    bitset<1> MemToreg;
+
+    
     bitset<32>RSData;
     bitset<32>RTData;
     bitset<32> SEImm;
     bitset<5> RDadd;
+    
     bitset<1>RegDist;
     bitset<1>ALUSrc;
     bitset<3>ALUOP;
 
-} DExreg;
+} IDExreg;
+
+
+struct {            
+    bitset<32> Instruction;
+    bitset<32> ALUresult;
+
+    bitset<1>Branch;
+    bitset<1>Zero;
+    bitset<1>MemRead;
+    bitset<1>MemWrite;
+
+    bitset<1> Regwwrite;
+    bitset<1> MemToreg;
+
+} EXMEMreg;
+
+struct {            
+    bitset<32> Instruction;
+
+    bitset<32> ALUresult;
+    bitset<32> Data_Mem;
+
+    bitset<1> Regwwrite;
+    bitset<1> MemToreg;
+    
+
+} MEMRBreg;
+
+
+
+
+
